ed92790a37737e9ede388daf713f53f69e231d61
srinu <srinu@SRINIVAS_ROY>
2022-01-16 20:28:55 +0530

    Add project files.

---
+++ Library/Controllers/HomeController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Library.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Entity db = new Entity();
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+
+
+
+        public ActionResult Welcome()
+        {
+            if (Session["UserId"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+
+        }
+
+   
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //POST: Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Login_Register _user)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = db.Login_Register.FirstOrDefault(s => s.Email == _user.Email);
+                if (check == null)
+                {
+                    _user.Password = GetMD5(_user.Password);
+                    db.Configuration.ValidateOnSaveEnabled = false;
+                    db.Login_Register.Add(_user);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Email already exists";
+                    return View();
+                }
+
+
+            }
+            return View();
+
+
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string Email,string Password)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                var f_password = GetMD5(Password);
+                var data =db.Login_Register.Where(s => s.Email.Equals(Email) && s.Password.Equals(f_password)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    Session["FullName"] = data.FirstOrDefault().FirstName +" "+ data.FirstOrDefault().LastName;
+                    Session["Email"] = data.FirstOrDefault().Email;
+                    Session["UserId"] = data.FirstOrDefault().UserId;
+                    return RedirectToAction("Welcome");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    //return RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
+
+
+        //Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("Login");
+        }
+
+
+
+        //create a string MD5
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+
+       
+    }
+}
---
+++ Library/Models/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Library.Models
+{
+    public class Entity : DbContext
+    {
+        public Entity() : base("Login_Register") { }
+        public DbSet<Login_Register> Login_Register { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<demoEntities>(null);
+            modelBuilder.Entity<Login_Register>().ToTable("Login_Register");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
